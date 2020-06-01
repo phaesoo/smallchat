684bfcc91eff6c88de8badacb5e2ac93e83209fa
phaesoo <phaesoo@gmail.com>
2020-06-02 01:29:01 +0900

    Follow simple example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,200 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
+
+const String _name = "HERE4YOU";
 
 void main() {
-  runApp(MyApp());
+  runApp(SmallChat());
 }
 
-class MyApp extends StatelessWidget {
+class SmallChat extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'smallChat',
+      home: ChatScreen(),
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class ChatScreen extends StatefulWidget {
+  ChatScreenState createState() => ChatScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  final List<ChatMessage> _message = <ChatMessage>[];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController _textController = TextEditingController();
+
+  bool _isComposing = false;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text("SmallChat"),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemCount: _message.length,
+                  itemBuilder: (_, index) => _message[index],
+                ),
+              ),
+              Divider(height: 1.0),
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                ),
+                child: _buildTextComposer(),
+              )
+            ],
+          ),
+        ));
+  }
+
+  // 사용자로부터 메시지를 입력받는 위젯 선언
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            // 텍스트 입력 필드
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                // 입력된 텍스트에 변화가 있을 때 마다
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                // 키보드상에서 확인을 누를 경우. 입력값이 있을 때에만 _handleSubmitted 호출
+                onSubmitted: _isComposing ? _handleSubmitted : null,
+                // 텍스트 필드에 힌트 텍스트 추가
+                decoration:
+                    InputDecoration.collapsed(hintText: "Send a message"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            // 전송 버튼
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              // 플랫폼 종류에 따라 적당한 버튼 추가
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("send"),
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null,
+                    )
+                  : IconButton(
+                      // 아이콘 버튼에 전송 아이콘 추가
+                      icon: Icon(Icons.send),
+                      // 입력된 텍스트가 존재할 경우에만 _handleSubmitted 호출
+                      onPressed: _isComposing
+                          ? () => _handleSubmitted(_textController.text)
+                          : null,
+                    ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  // 메시지 전송 버튼이 클릭될 때 호출
+  void _handleSubmitted(String text) {
+    // 텍스트 필드의 내용 삭제
+    _textController.clear();
+    // _isComposing을 false로 설정
+    setState(() {
+      _isComposing = false;
+    });
+    // 입력받은 텍스트를 이용해서 리스트에 추가할 메시지 생성
+    ChatMessage message = ChatMessage(
+      text: text,
+      // animationController 항목에 애니메이션 효과 설정
+      // ChatMessage은 UI를 가지는 위젯으로 새로운 message가 리스트뷰에 추가될 때
+      // 발생할 애니메이션 효과를 위젯에 직접 부여함
+      animationController: AnimationController(
+        duration: Duration(milliseconds: 700),
+        vsync: this,
+      ),
+    );
+    // 리스트에 메시지 추가
+    setState(() {
+      _message.insert(0, message);
+    });
+    // 위젯의 애니메이션 효과 발생
+    message.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    // 메시지가 생성될 때마다 animationController가 생성/부여 되었으므로 모든 메시지로부터 animationController 해제
+    for (ChatMessage message in _message) {
+      message.animationController.dispose();
+    }
+    super.dispose();
+  }
+}
+
+// 리스브뷰에 추가될 메시지 위젯
+class ChatMessage extends StatelessWidget {
+  final String text; // 출력할 메시지
+  final AnimationController animationController; // 리스트뷰에 등록될 때 보여질 효과
+
+  ChatMessage({this.text, this.animationController});
+
+  @override
+  Widget build(BuildContext context) {
+    // 위젯에 애니메이션을 발생하기 위해 SizeTransition을 추가
+    return SizeTransition(
+      // 사용할 애니메이션 효과 설정
+      sizeFactor:
+          CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      // 리스트뷰에 추가될 컨테이너 위젯
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              // 사용자명의 첫번째 글자를 서클 아바타로 표시
+              child: CircleAvatar(child: Text(_name[0])),
+            ),
+            Expanded(
+              // 컬럼 추가
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // 사용자명을 subhead 테마로 출력
+                  Text(_name, style: Theme.of(context).textTheme.subhead),
+                  // 입력받은 메시지 출력
+                  Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: Text(text),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
